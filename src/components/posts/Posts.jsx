20e7fc29103c68a022cc7dfcea189fad68c21de3
manishrohila <manishrohila0507@gmail.com>
2024-12-31 15:16:35 +0530

    Proxy changed

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,9 +4,11 @@ import "./posts.css";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
-      {posts.map((p) => (
-        <Post post={p} />
-      ))}
+      {Array.isArray(posts) && posts.length > 0 ? (
+        posts.map((p) => <Post key={p.id} post={p} />)
+      ) : (
+        <p>No posts available</p>
+      )}
     </div>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useContext, useRef } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
